e5733ea2f3d3c63c02c4b08a3e452287c6618f49
Mariana Barreiros <maridepp1@hotmail.com>
2017-04-26 11:41:45 -0300

    Inclusão de mais classes no modelo

--- Desktop/TrabalhoPWEB/financeiro/models.py
+++ Desktop/TrabalhoPWEB/financeiro/models.py
@@ -66,26 +66,97 @@ class FornecedorPessoaJuridica(PessoaJuridica):
     def __str__(self):
         return "%s " % (self.cliente)
 
-class LancamentosReceber(ClientePessoaFisica, ClientePessoaJuridica, Empresa):
-    identificador = models.PositiveIntegerField(primary_key=True)
+
+class LancamentosReceber(models.Model):
+    id_lancamentos_receber = models.PositiveIntegerField(primary_key=True)
     cliente = models.ForeignKey(ClientePessoaJuridica, ClientePessoaFisica, related_name="+")
     empresa = models.ForeignKey(Empresa, related_name="+")
     data_vencimento = models.DateField
-    data_emissao = models.dateField(help_text="Use o seguinte formato: <em>DD/MM/AAAA</em>")
+    data_emissao = models.DateField(help_text="Use o seguinte formato: <em>DD/MM/AAAA</em>")
     valor_titulo = models.FloatField(help_text="Use o seguinte formato: <em>DD/MM/AAAA</em>")
-    numero_documento = models.CharFiedl(max_length=20)
-    
+    numero_documento = models.CharField(max_length=20)
+
     def __str__(self):
         return "%s %s %s %s %s %s %s " % (self.identificador, self.cliente, self.empresa, self.data_vencimento, self.data_emissao, self.valor_titulo, self.numero_documento)
 
-class LancamentosPagar(FornecedorPessoaFisica, FornecedorPessoaJuridica, Empresa):
-    identificador = models.PositiveIntegerField(primary_key=True)
+
+class LancamentosPagar(models.Model):
+    id_lancamentos_pagar = models.PositiveIntegerField(primary_key=True)
     fornecedor = models.ForeignKey(FornecedorPessoaFisica, FornecedorPessoaJuridica, related_name="+")
     empresa = models.ForeignKey(Empresa, related_name="+")
     data_vencimento = models.DateField
-    data_emissao = models.dateField(help_text="Use o seguinte formato: <em>DD/MM/AAAA</em>")
-    valor_titulo = models.FloatField(help_text="Use o seguinte formato: <em>DD/MM/AAAA</em>")
-    numero_documento = models.CharFiedl(max_length=20)
-    
+    data_emissao = models.DateField(help_text="Use o seguinte formato: <em>DD/MM/AAAA</em>")
+    valor_titulo = models.FloatField
+    numero_documento = models.CharField(max_length=20)
+
     def __str__(self):
         return "%s %s %s %s %s %s %s " % (self.identificador, self.fornecedor, self.empresa, self.data_vencimento, self.data_emissao, self.valor_titulo, self.numero_documento)
+
+class FormasPagamentos(models.Model):
+    id_forma_pagamento = models.CharField(max_length= 10, primary_key=True)
+    descricao = models.CharField(max_length= 20)
+
+    def __str__(self):
+        return "%s %s " % (self.idenid_forma_pagamento, self.descricao)
+
+class BaixasReceber(models.Model):
+    id_baixa_receber = models.CharField(max_length  = 10, primary_key=True)
+    id_forma_pagamento = models.ForeignKey(FormasPagamentos, related_name="+")
+    id_lancamentos_receber = models.ForeignKey(LancamentosReceber, related_name="+")
+    banco = models.CharField(max_length  = 20)
+    disponibilidade = models.CharField(max_length = 20)
+    data_baixa = models.DateField(help_text="Use o seguinte formato: <em>DD/MM/AAAA</em>")
+    valor_pago = models.FloatField
+    residual = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return "%s %s %s %s %s %s %s %s  " % (self.id_baixa_receber, self.id_forma_pagamento, self.id_lancamentos_receber, self.banco, self.disponibilidade, self.data_baixa, self.valor_pago, self.residual)
+
+
+class BaixasPagar(models.Model):
+    id_baixa_pagar = models.CharField(max_length  = 10, primary_key=True)
+    id_forma_pagamento = models.ForeignKey(FormasPagamentos, related_name="+")
+    id_lancamentos_pagar = models.ForeignKey(LancamentosPagar, related_name="+")
+    banco = models.CharField(max_length  = 20)
+    disponibilidade = models.CharField(max_length  = 20)
+    data_baixa = models.DateField(help_text="Use o seguinte formato: <em>DD/MM/AAAA</em>")
+    valor_pago = models.FloatField
+    residual = models.CharField(max_length  = 20)
+
+    def __str__(self):
+        return "%s %s %s %s %s %s %s %s  " % (self.id_baixa_pagar, self.id_forma_pagamento, self.id_lancamentos_pagar, self.banco, self.disponibilidade, self.data_baixa, self.valor_pago, self.residual)
+
+class ContasBancarias(models.Model):
+    id_contas_bancarias = models.IntegerField(primary_key = True)
+    classificacao = models.CharField(max_length  = 20)
+    descricao = models.CharField(max_length=20)
+    numero_conta = models.CharField(max_length = 20)
+    numero_agencia = models.CharField(max_length = 20)
+    data_saldo_inicial = models.DateField(help_text="Use o seguinte formato: <em>DD/MM/AAAA</em>")
+    saldo_inicial = models.FloatField
+    caixa = models.CharField(max_length = 20)
+    banco = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return "%s %s %s %s %s %s %s %s %s   " % (self.id_contas_bancarias, self.classificacao, self.descricao, self.numero_conta, self.numero_agencia, self.data_saldo_inicial, self.saldo_inicial, self.caixa, self.banco)
+
+
+class PlanoDeContas(models.Model):
+    id_plano_contas = models.PositiveIntegerField(primary_key=True)
+    contas_bancarias = models.ForeignKey(ContasBancarias, related_name="+")
+    classificacao = models.CharField(max_length  = 20)
+    tipo_conta = models.CharField(max_length  = 20)
+    descricao = models.CharField(max_length  = 20)
+    caixa = models.CharField(max_length  = 20)
+    banco = models.CharField(max_length  = 20)
+    cliente = models.CharField(max_length  = 20)
+    fornecedor = models.CharField(max_length  = 20)
+    entrada_recurso = models.CharField(max_length  = 20)
+    saida_recurso = models.CharField(max_length  = 20)
+
+    def __str__(self):
+        return "%s %s %s %s %s %s %s %s %s %s %s   " % (self.id_plano_contas, self.contas_bancarias, self.classificacao, self.tipo_conta, self.descricao, self.caixa, self.banco, self.cliente, self.fornecedor, self.entrada_recurso, self.saida_recurso)
+
+
+
+
