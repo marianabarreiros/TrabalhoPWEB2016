1ac8b6301b8ec27e19c444640973baed86484479
marianabarreiros <maridepp1@hotmail.com>
2017-05-11 20:23:32 -0300

    atualização no model

--- financeiro/models.py
+++ financeiro/models.py
@@ -3,26 +3,24 @@ import datetime
 
 # Create your models here.
 class Pessoa(models.Model):
-    nome = models.CharField(max_length=50)
-    sobrenome = models.CharField(max_length=100)
-    endereco = models.CharField(max_length=50)
-    bairro = models.CharField(max_length=50)
-    municipio = models.CharField(max_length=50)
-    cep = models.CharField(max_length=20, help_text="Nao use caracteres especiais: <em>40353111</em")
-    uf = models.CharField(max_length=2)
-    telefone = models.CharField(max_length=20, help_text="Use o seguinte formato: <em>071999998888 ou 32220000</em>")
-    email = models.EmailField(max_length=50)
+    nome = models.CharField(max_length=50, default='')
+    sobrenome = models.CharField(max_length=100, default='')
+    endereco = models.CharField(max_length=50, default='')
+    bairro = models.CharField(max_length=50, default='')
+    municipio = models.CharField(max_length=50, default='')
+    cep = models.CharField(max_length=20, help_text="Nao use caracteres especiais: <em>40353111</em", default=' ')
+    uf = models.CharField(max_length=2, default='')
+    telefone = models.CharField(max_length=20, help_text="Use o seguinte formato: <em>071999998888 ou 32220000</em>", default='')
+    email = models.EmailField(max_length=50, default='')
 
-    def publish(self):
-        self.save()
+    class Meta:
+        abstract = True
 
-    def __str__(self):
-        return "%s %s %s %s %s %s %s %s %s" % (self.nome, self.sobrenome, self.endereco, self.bairro, self.municipio, self.cep, self.uf, self.telefone, self.email)
 
 class PessoaFisica(Pessoa):
     cpf = models.CharField(max_length=15, primary_key=True, help_text="Use o seguinte formato: <em>xxx.xxx.xxx-xx</em>")
     data_nascimento = models.DateField(help_text="Use o seguinte formato: <em>DD/MM/AAAA</em>", default=datetime.date.today)
-    funcao = models.CharField(max_length=50)
+    funcao = models.CharField(max_length=49)
 
     def __str__(self):
         return "%s %s %s " % (self.cpf, self.data_nascimento, self.funcao)
@@ -56,13 +54,13 @@ class Empresa(PessoaJuridica):
         return "%s %s %s" % (self.responsavel, self.inscricao_municipal, self.inscricao_estadual)
 
 class FornecedorPessoaFisica(PessoaFisica):
-    cliente = models.ForeignKey(PessoaFisica, related_name='+')
+    cliente_fpf = models.ForeignKey(PessoaFisica, related_name='+')
 
     def __str__(self):
         return "%s " % (self.cliente)
 
 class FornecedorPessoaJuridica(PessoaJuridica):
-    cliente = models.ForeignKey(PessoaJuridica, related_name='+')
+    cliente_fpj = models.ForeignKey(PessoaJuridica, related_name='+')
 
     def __str__(self):
         return "%s " % (self.cliente)
